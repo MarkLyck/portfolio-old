5866705b2d98bb4d6cba54f67bbecff291e64c4f
Mark Lyck <mark.lyck@gmail.com>
2016-09-03 17:42:12 -0500

    Added transition to footer hover effects

---
+++ app/scripts/components/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skills = React.createClass({
+  render() {
+    return (
+      <div className="skills">
+        <h2>Skills</h2>
+        <ul className="skill-list">
+          <li></li>
+        </ul>
+      </div>
+    )
+  }
+})
+
+export default Skills

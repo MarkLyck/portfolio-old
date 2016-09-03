64fcad5b1cda58fe218daa4462313629d441cd33
Mark Lyck <mark.lyck@gmail.com>
2016-09-03 17:26:43 -0500

    Fixed linkedin alignemnt

---
+++ app/scripts/components/QA.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const QA = React.createClass({
+  render() {
+    return (
+      <div className="content">
+        <h3>Q&A</h3>
+        <a>Where are you based?</a>
+        <a>Do you do freelance work?</a>
+      </div>
+    )
+  }
+})
+
+export default QA

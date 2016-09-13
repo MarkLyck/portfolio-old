ee02d9a5fe1895d9005253595c9e41798dd6f01e
Mark Lyck <mark.lyck@gmail.com>
2016-09-13 14:48:37 -0500

    changed currently exploring

--- app/scripts/components/CurrentlyExploring.js
+++ app/scripts/components/CurrentlyExploring.js
@@ -6,8 +6,8 @@ const CurrentlyExploring = React.createClass({
       <section className="currently-exploring">
         <h2>I'm currently exploring</h2>
         <div className="right">
-          <img src="/assets/images/skills/angular.png"/>
-          <h2>Angular</h2>
+          <img src="/assets/images/skills/redux.png"/>
+          <h2>Redux</h2>
         </div>
       </section>
     )

650d6f4702c037ed2a4887b0723ad1f2a895ed5f
Mark Lyck <mark.lyck@gmail.com>
2016-10-26 16:22:28 -0500

    Add Machine Learning

--- app/scripts/components/CurrentlyExploring.js
+++ app/scripts/components/CurrentlyExploring.js
@@ -6,8 +6,8 @@ const CurrentlyExploring = React.createClass({
       <section className="currently-exploring">
         <h2>I'm currently exploring</h2>
         <div className="right">
-          <img src="/assets/images/skills/angular.png"/>
-          <h2>Angular</h2>
+          <img src="/assets/images/skills/ai.png"/>
+          <h2>Machine learning</h2>
         </div>
       </section>
     )
